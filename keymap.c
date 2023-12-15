ec93b6c573b0da77a99a42fda3c2c1a4e8f81b32
John Liu <john-liu-einride@users.noreply.github.com>
2023-12-15 10:18:23 +0100

    fix: remove right WIN key on base layer
    The WIN key was interferring with the M key.

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|                       |------+------+------+------+------|
  * |   A  |   S  |   D  |   F  |   G  |                       |   H  |   J  |   K  |   L  | ' *  |
  * |------+------+------+------+------|                       |------+------+------+------+------|
- * | Shift| Ctrl | Alt  | Win  |      |                       |      | Win  | Alt  | Ctrl | Shift|
+ * | Shift| Ctrl | Alt  | Win  |      |                       |      |      | Alt  | Ctrl | Shift|
  * |   Z  |   X  |   C  |   V  |   B  |                       |   N  |   M  | , ;  | . :  | - _  |
  * `------'------'------+------+------+------.         ,------+------+------+------'------'------'
  *                      |      | Sym  | Shift|         |      | Num  |      |
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [QWERTY] = LAYOUT_split_3x5_3(
       KC_Q  ,  KC_W  ,  KC_E  ,   KC_R ,   KC_T ,                         KC_Y,  KC_U ,  KC_I ,   KC_O ,   KC_P ,
       KC_A  ,  KC_S  ,  KC_D  ,   KC_F ,   KC_G ,                         KC_H,  KC_J ,  KC_K ,   KC_L , SE_QUOT,
-      SFT_T(KC_Z), CTL_T(KC_X), ALT_T(KC_C), WIN_T(KC_V), KC_B ,          KC_N,  WIN_T(KC_M) ,ALT_T(KC_COMM), CTL_T(KC_DOT) , SFT_T(SE_MINS),
+      SFT_T(KC_Z), CTL_T(KC_X), ALT_T(KC_C), WIN_T(KC_V), KC_B ,          KC_N,  KC_M ,  ALT_T(KC_COMM), CTL_T(KC_DOT) , SFT_T(SE_MINS),
                          KC_TAB, LT(SYM,KC_BSPC), SFT_T(KC_ESC),          KC_ENT, LT(NUM,KC_SPC), KC_LWIN
     ),
 
